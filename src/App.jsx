47357f0221bac651b1c64c7498c468b57b80a38e
Pietro Weg <pietro.sera@compasso.com.br>
2022-06-15 14:43:28 -0300

    feat: global styles created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Header } from "./components/Header";
+
+import './global.css';
 
 export function App() {
 
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div>
+      <Header />
     </div>
   );
 };
