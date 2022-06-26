af5a0e31a280e60335552000db7506b9f70019f2
Pietro Weg <pietrows29@gmail.com>
2022-06-26 19:41:24 -0300

    fix: id for interation added

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -21,9 +21,7 @@ export function Post({ author, content, publishedAt }) {
   function handleCreateComment(e) {
     e.preventDefault();
 
-    setComments(prevState => [...prevState, textareaValue], () => {
-      console.log(comments)
-    });
+    setComments(prevState => [...prevState, textareaValue]);
 
     setTextareaValue('');
   }
@@ -46,9 +44,9 @@ export function Post({ author, content, publishedAt }) {
       <div className={styles.content}>
         {content.map(line => {
           if (line.type === 'paragraph') {
-            return <p>{line.content}</p>
+            return <p key={line.content}>{line.content}</p>
           } else if (line.type === 'link') {
-            return <p><a href="#">{line.content}</a></p>
+            return <p key={line.content}><a href="#">{line.content}</a></p>
           }
         })}
       </div>
@@ -68,7 +66,7 @@ export function Post({ author, content, publishedAt }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment content={comment} />
+          return <Comment key={comment} content={comment} />
         })}
       </div>
     </article>
